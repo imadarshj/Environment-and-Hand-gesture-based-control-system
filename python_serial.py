690d0b2f6e2602cace911b449a7b05655b9bcb2b
Adarsh Jaiswal <geniusadarsh3@gmail.com>
2020-09-14 11:28:57 +0530

    On branch master  Changes to be committed: 	modified:   ldr/ldr.ino 	modified:   python_serial.py

--- python_serial.py
+++ python_serial.py
@@ -1,12 +1,12 @@
 ##################################################################################################################################
-#                                                       Adarsh , Gunjan , Jishnu                                                               #
+#                                                       Adarsh , Aadithya                                                               #
 ##################################################################################################################################
 
 import serial 
 import sys
 import os
 
-ser = serial.Serial('/dev/ttyACM0',9600) 
+ser = serial.Serial('/dev/rfcomm4',9600) 
 
 path='/sys/class/backlight/intel_backlight/brightness' 
 
@@ -14,23 +14,28 @@ path='/sys/class/backlight/intel_backlight/brightness'
 
 while(1):
 	try:
-		value  = ser.readline().split()
-		val = int(value[0])
-		print (val) 
-
+		#value  = ser.readline().split()
+		value  = str(ser.readline())
+		value = value.split()
+		print (value) 
+		val = int(value[0][2:])
+		print(val)
+		
 		if(val <= 70):
 			os.system('xrandr --output eDP-1 --brightness 0.5')
-		if(val >=70 and val <= 200):
+		if(val >=70 and val <= 88):
 			os.system('xrandr --output eDP-1 --brightness 1.0')
-		if(val > 200):
+		if(val > 88):
 			os.system('xrandr --output eDP-1 --brightness 1.5')
-
-	
+		if(int(value[1])==1):
+			os.system("amixer set 'Master' 10%+")
+		if(int(value[1])==-1):
+			os.system("amixer set 'Master' 10%-")
 	except KeyboardInterrupt:
 		print('Interrupted')
 		break
-	# finally:
-		# ser.close()
+	finally:
+		ser.close()
 		# Just to take care of initial garbage values
 
 		
